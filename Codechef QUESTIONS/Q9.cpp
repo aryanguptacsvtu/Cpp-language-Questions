3f47663e3f3d98881ce273e75717ac8785200e9c
aryanguptacsvtu <148983503+aryanguptacsvtu@users.noreply.github.com>
2024-10-27 10:44:32 +0530

    Q9

---
+++ Codechef QUESTIONS/Q9.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b;
+        cin >> a >> b;
+        cout << a + b << endl;
+    }
+
+    return 0;
+}
+
+/*"Add Two Numbers":-
+Your task is very simple: given two integers A and B, write a program to add these two numbers and output the sum.
+
+Input Format:
+The first line contains an integer T, the total number of test cases.
+Then follow T lines, each line contains two integers, A and B.
+*/
