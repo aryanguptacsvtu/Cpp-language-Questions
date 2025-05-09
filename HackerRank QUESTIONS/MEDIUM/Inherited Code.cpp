43f2f7cf17e7d912e5491cd6126cedc189c8561c
Aryan Gupta <guptaaryan1578@gmail.com>
2025-05-09 10:25:20 +0530

    Inherited Code

---
+++ HackerRank QUESTIONS/MEDIUM/Inherited Code.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <exception>
+using namespace std;
+
+/* Define the exception here */
+class BadLengthException : public exception
+{
+private:
+    int n;
+    // override what() to return the letter length
+public:
+    BadLengthException(int length)
+    {
+        n = length;
+    }
+
+    int what()
+    {
+        return n;
+    }
+};
+
+bool checkUsername(string username)
+{
+    bool isValid = true;
+    int n = username.length();
+    if (n < 5)
+    {
+        throw BadLengthException(n);
+    }
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (username[i] == 'w' && username[i + 1] == 'w')
+        {
+            isValid = false;
+        }
+    }
+    return isValid;
+}
+
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        string username;
+        cin >> username;
+        try
+        {
+            bool isValid = checkUsername(username);
+            if (isValid)
+            {
+                cout << "Valid" << '\n';
+            }
+            else
+            {
+                cout << "Invalid" << '\n';
+            }
+        }
+        catch (BadLengthException e)
+        {
+            cout << "Too short: " << e.what() << '\n';
+        }
+    }
+    return 0;
+}
+
+/* "Inherited Code":-
+
+"Input Format":-
+The first line contains an integer, t, the number of test cases.
+Each of the t subsequent lines describes a test case as a single username string,u .
+
+"Output Format":-
+You are not responsible for directly printing anything to stdout. If your code is correct, the locked stub code in your editor will print either 'Valid' (if the username is valid), 'Invalid' (if the username is invalid), or Too short: n (where  is the length of the too-short username) on a new line for each test case.
+
+"Sample Input":-
+3
+Peter
+Me
+Arxwwz
+
+"Sample Output":-
+Valid
+Too short: 2
+Invalid
+*/
